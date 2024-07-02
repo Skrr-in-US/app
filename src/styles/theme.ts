91b08bc506cfd5260110ba57c1bd39a7818e523d
ubinquitous <ubinquitous1@gmail.com>
2024-07-02 12:26:40 +0900

    chore : define color theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  gray: '#00000070',
+  grey: '#3D3D3D',
+  background: '#F9F6F9',
+  shadow: '#676767',
+  white: '#FFFFFF',
+  black: '#000000',
+  quiz: {
+    pink: '#F55198',
+  },
+};
