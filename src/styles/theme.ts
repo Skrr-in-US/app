88fdc3141ede66593149a6bfdf8643bf07a534b4
ubinquitous <ubinquitous1@gmail.com>
2024-07-02 16:13:22 +0900

    chore : theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,8 +4,11 @@ export const theme = {
   background: '#F9F6F9',
   shadow: '#676767',
   white: '#FFFFFF',
+  question: '#F6F6F6',
+  whiteOpacity: '#FFFFFF70',
   black: '#000000',
   quiz: {
     pink: '#F55198',
+    purple: '#8E34FF',
   },
 };
