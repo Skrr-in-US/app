963b713f0ad2ebc524fbe4e91948aa2e36b12047
ubinquitous <ubinquitous1@gmail.com>
2024-07-10 21:43:11 +0900

    chore : queryclientProvider, jotai

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import RootNavigator from './src/navigation/RootNavigator';
+import {Provider} from 'jotai';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App(): React.JSX.Element {
-  return <RootNavigator />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <RootNavigator />
+      </Provider>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
